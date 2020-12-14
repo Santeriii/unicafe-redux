da0f055ed5d4f3dfc48e5c5768ab8057b65a7237
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2020-12-14 18:25:19 +0200

    exercises 6.1 and 6.2 done

--- src/reducer.js
+++ src/reducer.js
@@ -5,15 +5,36 @@ const initialState = {
 }
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action, state)
   switch (action.type) {
     case 'GOOD':
+      const changedGoodState = { 
+        ...state, 
+        good: state.good + 1
+      }
+      state = changedGoodState
       return state
     case 'OK':
+      const changedOkState = {
+        ...state,
+        ok: state.ok + 1
+      }
+      state = changedOkState
       return state
     case 'BAD':
+      const changedBadState = {
+        ...state,
+        bad: state.bad + 1
+      }
+      state = changedBadState
       return state
     case 'ZERO':
+      const resetState = {
+        good: 0,
+        ok: 0,
+        bad: 0
+      }
+      state = resetState
       return state
     default: return state
   }
